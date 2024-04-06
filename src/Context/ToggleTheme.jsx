618076d6fa1caa33038faacee84089a0deec72cd
NARD <glenardrefamonte1@gmail.com>
2024-04-06 09:12:15 +0800

    fix

--- src/Context/ToggleTheme.jsx
+++ src/Context/ToggleTheme.jsx
@@ -8,8 +8,14 @@ export const useTheme = () => {
 export const ToggleThemeProvider = ({ children }) => {
   const [toggleTheme, setToggleTheme] = useState(true);
 
+  const handleBodyBackground = () => {
+    const body = document.querySelector("body");
+    body.style.backgroundColor = toggleTheme ? "#f0f0f0" : "#141c2f";
+  };
+
   const handleToggleTheme = () => {
     setToggleTheme(prev => !prev);
+    handleBodyBackground();
   };
   return (
     <ThemeContext.Provider value={{ handleToggleTheme, toggleTheme }}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <>
       <main
-        className={`flex items-center justify-center h-[900px] max-lg:px-3 w-full   ${
-          toggleTheme ? "bg-[#141c2f]" : "bg-white"
-        }`}
+        className={`flex items-center justify-center h-[900px] max-lg:px-3 w-full   
+       
+        `}
       >
         <section className="w-[600px] h-[750px] text-[#141c2f]  max-md:w-full max-xs:mt-10 ">
           <SearchInput />
