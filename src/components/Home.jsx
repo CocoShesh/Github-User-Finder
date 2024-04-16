4bed776f45d2e39635b8cd52e5dfad7dfa01c321
NARD <glenardrefamonte1@gmail.com>
2024-04-16 13:13:38 +0800

    remove the imported context and conditonal rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import SearchInput from "./Search/SearchInput";
-import { useTheme } from "../Context/ToggleTheme";
 const Home = () => {
-  const { toggleTheme } = useTheme();
   return (
     <>
       <main
-        className={`flex items-center justify-center h-[900px] max-lg:px-3 w-full   
+        className={`flex items-center justify-center h-[900px] max-lg:px-3 w-full   dark:bg-[#f5f7ff] bg-[#141c2f]  
        
         `}
       >
