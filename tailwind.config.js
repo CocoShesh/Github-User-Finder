d5d1beec3f806b923192f40fdeccc5476004d89b
NARD <glenardrefamonte1@gmail.com>
2024-04-16 13:12:20 +0800

    Added the darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "space-mono": ["Space Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
